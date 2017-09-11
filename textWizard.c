a148ca72357031839654293bf12906ae4e9ed51b
Darlene Evans <daelevan@tesla.cs.iupui.edu>
2017-09-11 00:04:51 -0400

    almost done

--- textWizard.c
+++ textWizard.c
@@ -9,35 +9,48 @@
 #include <stdio.h>
 #include <string.h>
 
+// assigning variables to empty strings or values
 char input[0];
-int wordLength = (strlen(input));
+int wordLength = 0;
 int beginning = 0;
 int placeHolder = 0;
-int end = (wordLength-1);
+int end = 0;
 
 //   This function uses recursion to reverse a word in C.
 void  reversedWord (char input [], int size)
 {
-
-	do { 
-		beginning = placeHolder;
-		beginning = end;
-		end = placeHolder;
+	// if the the first half of the word gets bigger than the last half of the word then it will print the reversedWord
+	if (beginning >= end){ 
+		printf("%s\n", input);	
+	// If not, the places in the array where the entered word is stored will switch
+	} else {
+		// sets the beginning position in the array to a placeholder so that the value is not lost	
+		input[placeHolder] = input[beginning];
+		// the spot where the end half is replaced with the corresponding value from the beginning
+		input[beginning] = input[end];
+		// the held value from the beginning is placed into the corresponding spot at the end
+		input[end] = input[placeHolder];
 		
-	} while (beginning >= end);	
-
+		// adds one or subtracts one to move inward
+		beginning = beginning+1;
+		end = end-1;
+		
+		// repeats the function	
+		reversedWord(input, wordLength);
+	};
 }
 
 //   This is our main method that is responsible for running our program.
 int main ()
 {
-	//char input[0];
-	//int wordLength;
-	//wordLength = (sizeof(input)/(sizeof(char)));
-	//wordLength = (strlen(input));
+	// stores user input and length of the entered word
+	wordLength = (sizeof(input)/(sizeof(char)));
+
 	printf("Enter a word to reverse:\n");
   scanf("%s", input);
 
+	end = strlen(input)-1;
+	
 	reversedWord(input, wordLength);
 	
 	return(0);
