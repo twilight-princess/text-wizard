920dd3515fb0b14c28fbe2335e120de84b04660f
Darlene Evans <daelevan@tesla.cs.iupui.edu>
2017-09-11 22:16:18 -0400

    I'm not sure exactly is wanted in the algorith

--- textWizard.c
+++ textWizard.c
@@ -11,7 +11,6 @@
 
 // assigning variables to empty strings or values
 char input[0];
-int wordLength = 0;
 int beginning = 0;
 int placeHolder = 0;
 int end = 0;
@@ -21,37 +20,50 @@ void  reversedWord (char input [], int size)
 {
 	// if the the first half of the word gets bigger than the last half of the word then it will print the reversedWord
 	if (beginning >= end){ 
-		printf("%s\n", input);	
+		printf("%s\n", input);
+  
+		char yesOrNo[0];
+		char yes[] = "Y";
+	
+		printf("Would you like to continue? (Y/N)\n");
+		scanf("%s", yesOrNo);
+		
+		if (yesOrNo[0] != yes[0]){
+			printf("Okay, bye!\n");
+		} else {
+    
+			beginning = 0;
+    	placeHolder = 0;
+    	end = 0;
+			main ();
+		};	
 	// If not, the places in the array where the entered word is stored will switch
 	} else {
-		// sets the beginning position in the array to a placeholder so that the value is not lost	
-		input[placeHolder] = input[beginning];
-		// the spot where the end half is replaced with the corresponding value from the beginning
+		
+		placeHolder = input[beginning];
 		input[beginning] = input[end];
-		// the held value from the beginning is placed into the corresponding spot at the end
-		input[end] = input[placeHolder];
+		input[end] = placeHolder;
 		
 		// adds one or subtracts one to move inward
 		beginning = beginning+1;
 		end = end-1;
 		
 		// repeats the function	
-		reversedWord(input, wordLength);
+		reversedWord(input, end);
 	};
 }
 
 //   This is our main method that is responsible for running our program.
 int main ()
 {
+	
 	// stores user input and length of the entered word
-	wordLength = (sizeof(input)/(sizeof(char)));
-
 	printf("Enter a word to reverse:\n");
   scanf("%s", input);
 
 	end = strlen(input)-1;
-	
-	reversedWord(input, wordLength);
+
+	reversedWord(input, end);
 	
 	return(0);
 			
