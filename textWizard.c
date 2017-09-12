e279f8d35dc10d002281a808f850e50fcd41fd79
Darlene Evans <daelevan@tesla.cs.iupui.edu>
2017-09-11 22:27:50 -0400

    final copy

--- textWizard.c
+++ textWizard.c
@@ -21,25 +21,30 @@ void  reversedWord (char input [], int size)
 	// if the the first half of the word gets bigger than the last half of the word then it will print the reversedWord
 	if (beginning >= end){ 
 		printf("%s\n", input);
-  
+ 		
+		// if user enters Y then it will match the variable yes  
 		char yesOrNo[0];
 		char yes[] = "Y";
 	
 		printf("Would you like to continue? (Y/N)\n");
 		scanf("%s", yesOrNo);
 		
+		// if they do not enter Y then it will quit
 		if (yesOrNo[0] != yes[0]){
 			printf("Okay, bye!\n");
 		} else {
-    
+    	
+			// if they do enter yes then the variables are reset and the main function restarts
 			beginning = 0;
     	placeHolder = 0;
     	end = 0;
 			main ();
 		};	
-	// If not, the places in the array where the entered word is stored will switch
+		
+	// the places in the array where the entered word is stored will switch
 	} else {
 		
+		// temporarily stores beginning in a variable then places that into the end
 		placeHolder = input[beginning];
 		input[beginning] = input[end];
 		input[end] = placeHolder;
@@ -60,7 +65,8 @@ int main ()
 	// stores user input and length of the entered word
 	printf("Enter a word to reverse:\n");
   scanf("%s", input);
-
+	
+	// sets variable end equal to position in the array
 	end = strlen(input)-1;
 
 	reversedWord(input, end);
